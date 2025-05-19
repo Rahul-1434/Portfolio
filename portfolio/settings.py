f6e5c7d10ebd2f33a73043d62124a3d6baa6509f
Rahul-1434 <157436820+Rahul-1434@users.noreply.github.com>
2025-05-19 11:33:00 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8qje)zj)cq)z6n1117!9u%i(t+-o_ygnu-kh&pldr6oa*mgo8j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
